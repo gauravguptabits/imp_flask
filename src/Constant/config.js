cf942777773321d9bb00bbca167198707980c80f
Gaurav (BITS) <2019ad04038@wilp.bits-pilani.ac.in>
2021-02-16 19:10:03 +0530

    updated config file

--- src/Constant/config.js
+++ src/Constant/config.js
@@ -2,16 +2,16 @@
 let BASE_URL_PATH;
 
 if (process.env.REACT_APP_NODE_ENV === 'production') {
-  BASE_URL_PATH = "http://localhost:5000/"
+  BASE_URL_PATH = "https://shielded-crag-40471.herokuapp.com/"
 } 
 else if (process.env.REACT_APP_NODE_ENV === 'staging') {
-  BASE_URL_PATH = "http://localhost:5000/"
+  BASE_URL_PATH = "https://shielded-crag-40471.herokuapp.com/"
 } 
 else if (process.env.REACT_APP_NODE_ENV === 'qa') {
-  BASE_URL_PATH = "http://localhost:5000/"
+  BASE_URL_PATH = "https://shielded-crag-40471.herokuapp.com/"
 } 
 else {
-  BASE_URL_PATH = 'http://localhost:5000/'
+  BASE_URL_PATH = 'https://shielded-crag-40471.herokuapp.com/'
 }
 
 
